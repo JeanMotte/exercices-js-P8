a72d5f44e0bb2caf2f1cca8bb80f7950d135248a
Jean Motte <jijimotte@gmail.com>
2025-04-11 18:33:43 +0200

    INIT

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,14 @@
 //votre code ici
 
-export default pairNumbers
+export default function pairNumbers(startRange, endRange) {
+    let result = [];
+    for (let i = startRange; i <= endRange; i++) {
+        if (i % 2 === 0) {
+            result.push(i);
+        }
+    }
+    return result.join(",");
+}
+
+console.log(pairNumbers(5, 16));
+
